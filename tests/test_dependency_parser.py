78c3f723decceb48b2caf012fad10e0866852cad
ayub <kokabi.ayub@gmail.com>
2026-03-29 23:28:24 +0330

    skip testing dependency parser if Java is not installed

--- tests/test_dependency_parser.py
+++ tests/test_dependency_parser.py
@@ -1,9 +1,16 @@
+import shutil
+
+import pytest
+
 from hazm import word_tokenize
 
 
 class TestDependencyParser:
 
     def test_parse(self:"TestDependencyParser", dependency_parser):
+        if not shutil.which("java"):
+            pytest.skip("Java is not installed. Skipping MaltParser test.")
+
         sent = "آدمک آخر دنیاست بخند."
         parsed_tree = dependency_parser.parse(word_tokenize(sent))
         actual = parsed_tree.to_conll(10).split("\n")[0]
